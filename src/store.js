6b755a42b6514c9063911cc57166fd92340c8a4f
Abdul-Qoyyum <nurudeen96qoyyum@gmail.com>
2020-03-26 10:48:01 -0400

    Added redux store

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import thunk from 'redux-thunk';
+import promise from "redux-promise-middleware";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, applyMiddleware } from 'redux';
+
+import reducers from './reducers';
+
+const middleware =  [thunk, promise];
+
+const store = createStore(reducers,composeWithDevTools(applyMiddleware(...middleware)));
+
+export default store;
