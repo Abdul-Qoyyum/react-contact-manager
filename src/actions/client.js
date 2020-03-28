c2efafe3d8eecdd6116c74682f03dd46e9beb59c
Abdul-Qoyyum <nurudeen96qoyyum@gmail.com>
2020-03-28 10:09:20 -0400

    Modified contact list reducer

--- src/actions/client.js
+++ src/actions/client.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 
 export const client = axios.create({
 
-    baseURL: "http://localhost:3030",
+    baseURL: 'http://localhost:3030',
 
     headers : {
         "Content-Type" : "application/json"
     }
 
 });
+
