55eb05033591e2311988bdfcc9e654349b44e6da
Abdul-Qoyyum <nurudeen96qoyyum@gmail.com>
2020-03-29 15:38:26 -0400

    Modified contact-list-reducer

--- src/reducers/contact-list-reducer.js
+++ src/reducers/contact-list-reducer.js
@@ -7,10 +7,10 @@ const defaultState = {
 export const contactListReducer = (state = defaultState, action={}) => {
     switch (action.type) {
         case FETCH_CONTACTS : {
-            return {...state, contacts : action.payload}
+            return { ...state, contacts : action.payload }
         }
         case FETCH_CONTACTS_FULFILLED : {
-            return {...state, contacts: action.payload.data.data || action.payload.data }
+            return { ...state, contacts : action.payload.data.data || action.payload.data }
         }
         default :
             return state;
